3fdbbee61d9044cd960d84bf0f84099fa1a4f331
DESKTOP-KSB7AKQ\jeff1212 <zeratool0207@naver.com>
2023-03-03 05:13:02 +0900

    0503 game project proceeding...

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
  'use strict'
 
+ /*
  console.log(document.querySelector('.message').textContent);
 
  document.querySelector('.message').textContent = `🎉🎉
@@ -10,3 +11,14 @@ document.querySelector('.score').textContent = 10;
 
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+
+*/
+
+document.querySelector('.check').addEventListener('click',function(){
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+
+    if (!guess) {
+        document.querySelector('.message').textContent = '😨 No number!';
+    }
+})
