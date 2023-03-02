67952f00ed2d58be6a604723e8dea0b8d2cbdf67
DESKTOP-KSB7AKQ\jeff1212 <zeratool0207@naver.com>
2023-03-03 05:31:37 +0900

    0303 0529

--- script.js
+++ script.js
@@ -14,11 +14,35 @@ console.log(document.querySelector('.guess').value);
 
 */
 
+const secretNumber = Math.trunc(Math.random() * 20 ) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click',function(){
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess, typeof guess);
 
     if (!guess) {
         document.querySelector('.message').textContent = '😨 No number!';
+    } else if ( guess === secretNumber ) {
+        document.querySelector('.message').textContent = `🎉🎉 Correct Number!!`;
+    } else if ( guess > secretNumber) {
+        if ( score > 1) {
+            document.querySelector('.message').textContent = `😢😢 Too high!!`;
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = `😱😱You Lost Game!!`;
+            document.querySelector('.score').textContent = 0;
+        }
+    } else if ( guess < secretNumber) {
+        if ( score > 1) {
+            document.querySelector('.message').textContent = `😢😢 Too Low!!`;
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = `😱😱You Lost Game!!`;
+            document.querySelector('.score').textContent = 0;
+        }
     }
-})
+});
