9985cd8c2be27cbabf557c4af23a810981a26acd
DESKTOP-KSB7AKQ\jeff1212 <zeratool0207@naver.com>
2023-03-04 07:53:45 +0900

    game proceeding...

--- script.js
+++ script.js
@@ -14,13 +14,12 @@ console.log(document.querySelector('.guess').value);
 
 */
 
-const secretNumber = Math.trunc(Math.random() * 20 ) + 1;
+let secretNumber = Math.trunc(Math.random() * 20 ) + 1;
 let score = 20;
 document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click',function(){
     const guess = Number(document.querySelector('.guess').value);
-    console.log(guess, typeof guess);
 
     // When there is no input
     if (!guess) {
@@ -30,9 +29,6 @@ document.querySelector('.check').addEventListener('click',function(){
         document.querySelector('.message').textContent = `🎉🎉 Correct Number!!`;
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector('.number').style.width = '30rem';
-        
-
-
     // When guess is too high
     } else if ( guess > secretNumber) {
         if ( score > 1) {
@@ -54,4 +50,18 @@ document.querySelector('.check').addEventListener('click',function(){
             document.querySelector('.score').textContent = 0;
         }
     }
+});
+
+document.querySelector('.again').addEventListener('click',function(){
+    // again 버튼 클릭시 발생
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20 ) + 1;
+    document.querySelector('.number').textContent = '?';
+
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.guess').textContent = '';
+
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
 });
