c46ffe3adcd6aeb8a6c7becff2cf73ef9dc61b6f
DESKTOP-KSB7AKQ\jeff1212 <zeratool0207@naver.com>
2023-03-04 08:03:39 +0900

    0304 game 1st finish

--- script.js
+++ script.js
@@ -16,7 +16,10 @@ console.log(document.querySelector('.guess').value);
 
 let secretNumber = Math.trunc(Math.random() * 20 ) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
+let highscore = 0;
+
+
+document.querySelector('.number').textContent = '?';
 
 document.querySelector('.check').addEventListener('click',function(){
     const guess = Number(document.querySelector('.guess').value);
@@ -26,9 +29,16 @@ document.querySelector('.check').addEventListener('click',function(){
         document.querySelector('.message').textContent = '😨 No number!';
     // When palyer wins
     } else if ( guess === secretNumber ) {
+        document.querySelector('.number').textContent = secretNumber;
         document.querySelector('.message').textContent = `🎉🎉 Correct Number!!`;
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector('.number').style.width = '30rem';
+
+        if ( score > highscore ) {
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
+        }
+
     // When guess is too high
     } else if ( guess > secretNumber) {
         if ( score > 1) {
